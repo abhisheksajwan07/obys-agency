0c62f16db9b76d7d4f969ae0ddce3c3db74f0433
abhishek sajwan <abhisheksajwan2004@gmail.com>
2024-10-09 00:36:50 +0530

    page1 done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ tl.from(".line h1",{
   stagger:0.2,
   duration:0.6,
   delay:0.2
-})
+});
 tl.from("#line-part-1",{
   opacity:0,
   onStart:function(){
@@ -30,18 +30,43 @@ tl.to(".line h2",{
 });
 tl.to("#loader",{
   opacity:0,
-  duration:0.4,
+  duration:0.6,
   delay:4
+  
 });
 tl.from("#page1",{
   delay:0.2,
-  y:1600,
+  y:1500,
   opacity:0,
-  duration:0.4,
+  duration:0.6,
   ease:Power4
 });
 tl.to("#loader",{
   display:"none"
 });
+tl.from("#nav",{
+  opacity:0
+});
+tl.from(["#hero1 h1","#hero2 h1","#hero3 h2","#hero4 h1"],{
+  y:120,
+  stagger:0.2
+}
+)
+
+ 
+
 }
 loadingAnimation()
+
+function cursorAnimation(){
+  document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+      left:dets.x,
+      top:dets.y,
+    });
+  });
+  
+  Shery.makeMagnet("#nav-part2 h4", {});
+
+}
+cursorAnimation();
